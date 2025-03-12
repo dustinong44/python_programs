46fcce4226d55c5e87cfa3d079e05a1ab4f57b84
dustinong44 <dustinong44@gmail.com>
2025-03-12 20:45:10 +0800

    first commit

---
+++ number_sum/number_sum/number_sum.py
@@ -0,0 +1,14 @@
+
+while True:
+    print ("Good Day!!")
+    first_num = float (input ("Please enter your first addends:"))
+    second_num = float (input ("Please enter your second addends:"))
+
+    sum_num = first_num + second_num
+
+    print ("The total of the two numbers is", sum_num)
+
+    decision = input ("Do you want to input another pair of number? (yes/no):")
+    if decision != "yes":
+        print  ("Thank you")
+        break 
