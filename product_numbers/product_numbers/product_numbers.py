258222b3d62ba63654341bcdebaa028b59d94950
dustinong44 <dustinong44@gmail.com>
2025-03-12 21:09:52 +0800

    first commit

---
+++ product_numbers/product_numbers/product_numbers.py
@@ -0,0 +1,14 @@
+
+while True:
+    print ("Good Day!!")
+    first_num = float (input ("Please enter your multiplier:"))
+    second_num = float (input ("Please enter your multiplicand:"))
+
+    product_num = first_num * second_num
+
+    print ("The product of the two numbers is", product_num)
+
+    decision = input ("Do you want to input another pair of number? (yes/no):")
+    if decision != "yes":
+        print  ("Thank you")
+        break 
