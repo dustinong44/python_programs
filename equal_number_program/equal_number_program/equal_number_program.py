3b0bd7a9160b2e0e3c0b4bcdf78dccb17da2a768
dustinong44 <dustinong44@gmail.com>
2025-03-12 20:00:13 +0800

    first commit

---
+++ equal_number_program/equal_number_program/equal_number_program.py
@@ -0,0 +1,19 @@
+
+while True:
+
+    print ("Good Day!")
+
+    first_num=float (input("Please Enter your First Number:"))
+    second_num=float (input("Please Enter your Second Number:"))
+
+    if first_num == second_num:
+        print ("The Given Numbers are Equal")
+
+    else:
+        print ("The Given Numbers are Not Equal")
+
+
+    decision = input ("Do you want to input another pair of number? (yes/no):")
+    if decision != "yes":
+        print  ("Thank you")
+        break 
