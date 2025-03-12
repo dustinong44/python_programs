f90adb32f2fea89ae4e538cd9d2792c085d6cbe1
dustinong44 <dustinong44@gmail.com>
2025-03-12 13:10:10 +0800

    First Commit

--- bigger_number_program/bigger_number_program.py
+++ bigger_number_program/bigger_number_program.py
@@ -1,1 +1,10 @@
 
+print ("Good Day")
+first_number = float (input("Enter your first number:"))
+second_number = float (input("Enter your second number:"))
+
+if first_number > second_number:
+    print (first_number)
+
+else:
+    print (second_number)
