ad3a789c282089c1b8ea2b942747b7fcefea1922
dustinong44 <dustinong44@gmail.com>
2025-03-12 21:47:58 +0800

    first commit

---
+++ exponent_program/exponent_program/exponent_program.py
@@ -0,0 +1,13 @@
+while True:
+    print ("Good Day!!")
+    first_num = float (input ("Please enter your First Number:"))
+    second_num = float (input ("Please enter your Second Number:"))
+
+    result_num = first_num  ** second_num
+
+    print ("The result of the exponentiation of two numbers is", result_num)
+
+    decision = input ("Do you want to input another pair of number? (yes/no):")
+    if decision != "yes":
+        print  ("Thank you")
+        break 
