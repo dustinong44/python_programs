d024370c24eadb6f15e4ffdc890347fc38eb921a
dustinong44 <dustinong44@gmail.com>
2025-03-12 13:20:03 +0800

    last commit

--- bigger_number_program/bigger_number_program.py
+++ bigger_number_program/bigger_number_program.py
@@ -1,10 +1,17 @@
 
-print ("Good Day")
-first_number = float (input("Enter your first number:"))
-second_number = float (input("Enter your second number:"))
+while True:
+    print ("Good Day")
+    first_number = float (input("Enter your first number:"))
+    second_number = float (input("Enter your second number:"))
 
-if first_number > second_number:
-    print (first_number)
+    if first_number > second_number:
+        print (first_number)
 
-else:
-    print (second_number)
+    else:
+        print (second_number)
+
+
+    decision = input ("Do you want enter another pair of numbers? (yes/no)")
+    if decision != "yes":
+        print ("Thank You!!")
+        break
