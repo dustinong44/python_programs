18eb3e6263727dc571d35bf3c43617f5a042d01c
dustinong44 <dustinong44@gmail.com>
2025-03-13 20:44:26 +0800

    first commit

---
+++ odd_numbers_counter/odd_numbers_counter/odd_numbers_counter.py
@@ -0,0 +1,12 @@
+while True:
+    for a in range (0, 10):
+        user_input = input ("Please enter your number:")
+        
+        
+        
+        if a % 2 != 0:
+            print ("There are" ,a )
+
+    decision = input ("Do you want to enter another set of numbers? (yes/no):")
+    if decision != "yes":
+        break
