9d6975f98709542e3b3b06e65ea154178eb10c19
dustinong44 <dustinong44@gmail.com>
2025-03-13 20:49:02 +0800

    fixing the iteration and odd numbers checking

--- odd_numbers_counter/odd_numbers_counter/odd_numbers_counter.py
+++ odd_numbers_counter/odd_numbers_counter/odd_numbers_counter.py
@@ -2,10 +2,12 @@ while True:
     for a in range (0, 10):
         user_input = input ("Please enter your number:")
         
+        odd_number = 0
         
-        
-        if a % 2 != 0:
-            print ("There are" ,a )
+        if a % 2 == 0:
+         odd_number += 1
+            
+        print ("There are", odd_number, "odd numbers on the set of numbers")
 
     decision = input ("Do you want to enter another set of numbers? (yes/no):")
     if decision != "yes":
