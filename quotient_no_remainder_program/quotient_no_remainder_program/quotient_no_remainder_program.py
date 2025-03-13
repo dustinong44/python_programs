7766021f91b6843beaca5b77ba5812f7f158c7d0
dustinong44 <dustinong44@gmail.com>
2025-03-13 08:55:59 +0800

    first commit

---
+++ quotient_no_remainder_program/quotient_no_remainder_program/quotient_no_remainder_program.py
@@ -0,0 +1,15 @@
+while True:
+    print ("Good Day!!")
+    first_num = float (input ("Please enter your dividend:"))
+    second_num = float (input ("Please enter your divisor:"))
+
+    quotient_num = first_num // second_num
+
+    print ("The quotient of the two numbers without decimal point is", quotient_num)
+
+    decision = input ("Do you want to input another pair of number? (yes/no):")
+    if decision != "yes":
+        print  ("Thank you")
+        break 
+
+
