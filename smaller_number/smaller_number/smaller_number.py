3bd54b429648d2dfbcb472772686d4043de6cd68
dustinong44 <dustinong44@gmail.com>
2025-03-13 22:06:59 +0800

    final version: fixed versions

--- smaller_number/smaller_number/smaller_number.py
+++ smaller_number/smaller_number/smaller_number.py
@@ -1,8 +1,8 @@
 
 while True:
     print ("Good Day")
-    first_number = float (input("Enter your first number:"))
-    second_number = float (input("Enter your second number:"))
+    first_number = float(input("Enter your first number:"))
+    second_number = float(input("Enter your second number:"))
 
     if first_number < second_number:
         print (first_number)
