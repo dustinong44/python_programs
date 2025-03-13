b637899686a4947ea2fe09bf595aa1db78de5805
dustinong44 <dustinong44@gmail.com>
2025-03-13 08:45:39 +0800

    first commit

---
+++ difference_of_numbers/difference_of_numbers/difference_of_numbers.py
@@ -0,0 +1,15 @@
+
+while True:
+    print ("Good Day!!")
+    first_num = float (input ("Please enter your subtrahend:"))
+    second_num = float (input ("Please enter your minuend:"))
+
+    difference_of_num = first_num  - second_num
+
+    print ("The total of the two numbers is", difference_of_num)
+
+    decision = input ("Do you want to input another pair of number? (yes/no):")
+    if decision != "yes":
+        print  ("Thank you")
+        break 
+
