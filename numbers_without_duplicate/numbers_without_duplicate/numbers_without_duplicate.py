1f34f1d9722017a50d3ec251da64eb8a97d1e30f
dustinong44 <dustinong44@gmail.com>
2025-03-14 13:47:18 +0800

    get inputs and lot of missing code

---
+++ numbers_without_duplicate/numbers_without_duplicate/numbers_without_duplicate.py
@@ -0,0 +1,5 @@
+total = 0
+
+for i in range (0,10):
+     user_input = int(input("enter number"))
+     total += user_input
--- with_the_most_duplicate/with_the_most_duplicate/with_the_most_duplicate.py
+++ with_the_most_duplicate/with_the_most_duplicate/with_the_most_duplicate.py
@@ -10,5 +10,14 @@ while True:
 max_count = 0
 most_duplicated_number = None
 
+for num in input_list:
+    count = input_list.count(num)
+    if count > max_count:
+        max_count = count
+        most_duplicated_number = num
 
+if max_count > 1:
+    print(f"The number with the most duplicates is: {most_duplicated_number} (appears {max_count} times).")
+else:
+    print("No duplicates found.")
 
