df8768f38dd40066ff543b190808888df9820f53
dustinong44 <dustinong44@gmail.com>
2025-03-14 14:11:38 +0800

    working version, final version

--- numbers_without_duplicate/numbers_without_duplicate/numbers_without_duplicate.py
+++ numbers_without_duplicate/numbers_without_duplicate/numbers_without_duplicate.py
@@ -1,5 +1,17 @@
-total = 0
 
-for i in range (0,10):
-     user_input = int(input("enter number"))
-     total += user_input
+total = 0
+input_list = []
+
+
+for i in range(10):
+    user_input = int(input("Enter number: "))
+    input_list.append(user_input)
+    total += user_input
+
+
+duplicates = []
+for num in input_list:
+    if input_list.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
+print("Numbers with duplicates:", duplicates if duplicates else "No duplicates found")
