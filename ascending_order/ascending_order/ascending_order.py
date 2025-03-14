74ee5d863bb92e4e780b3e4c3ea32577b4e23e08
dustinong44 <dustinong44@gmail.com>
2025-03-14 12:57:30 +0800

    working but subject to revisions

---
+++ ascending_order/ascending_order/ascending_order.py
@@ -0,0 +1,12 @@
+input_list = []
+
+while True: 
+    try:
+        input_num = float(input("Enter your numbers: "))
+        input_list.append(input_num)
+    except ValueError:
+        break
+
+    input_list.sort()
+
+    print(input_list)
