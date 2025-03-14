92e09b5daa0eaa52355d18717ac9dbd1d2eda801
dustinong44 <dustinong44@gmail.com>
2025-03-14 15:17:38 +0800

    final and revised version

--- unique_or_duplicate/unique_or_duplicate/unique_or_duplicate.py
+++ unique_or_duplicate/unique_or_duplicate/unique_or_duplicate.py
@@ -4,12 +4,13 @@ while True:
     try:
         input_num = float(input("Enter your numbers (Enter a non-numeric value to proceed):"))
         
+        if input_num in input_list:
+            print("Duplicate")
+        else:
+            print("Unique")
+        
         input_list.append(input_num)
     except ValueError:
         break
-if input_num in input_list:
-            print("Duplicate")
-else:
-       print("Unique")
 
-print("All numbers entered:", input_list)
+  
